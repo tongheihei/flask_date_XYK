b6e4f0aa696e48c40a8a1087c2784b16fd29f118
tongheihei <792886480@qq.com>
2021-10-28 18:05:49 +0800

    8.6

---
+++ app/auth/forms.py
@@ -0,0 +1,28 @@
+from app import email
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,BooleanField, SubmitField,ValidationError
+from wtforms.validators import DataRequired, Length ,Email,Regexp,EqualTo
+from ..models import User
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',validators=[DataRequired(), Length(1,64),Email()])
+    password = PasswordField('Password', validators = [DataRequired()])
+    remember_me = BooleanField('Keep me logged in')
+    sumbmit = SubmitField('Log In')
+class RegistrationForm(FlaskForm):
+    email = StringField('Email',validators=[DataRequired(),Length(1,64),Email()])
+    username = StringField('Username', validators=[
+        DataRequired(),Length(1,64),Regexp(
+            '^[A-Za-z][A-Za-z0-9_.]*$',0,
+            '用户名只能用英文字母，数字，或者下划线'
+        )])
+    password = PasswordField('Password',validators=[DataRequired(), EqualTo('password2',message ='两次密码必须相同')])
+    password2 = PasswordField('Confirm Password',validators = [DataRequired()])
+    submit = SubmitField('Register')
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email has already used')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username has already used')
---
+++ app/auth/views.py
@@ -0,0 +1,62 @@
+from flask import render_template,redirect,request,url_for,flash
+from flask_login import login_user,login_required,logout_user,current_user
+from . import auth
+from ..models import User
+from .forms import LoginForm,RegistrationForm
+from .. import db
+from ..email import send_email
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email = form.email.data.lower()).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            next = request.args.get('next')
+            if next is None or not next.startswith('/'):
+                next = url_for('main.index')
+            return redirect(next)
+        flash('帐号或者密码错误')
+    return render_template('auth/login.html', form = form)
+@auth.route('logout')
+@login_required
+def logout():
+    logout_user()
+    flash('you have log out')
+    return redirect(url_for('main.index'))
+
+@auth.route('/register',methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data,
+                    username = form.username.data,
+                    password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        token = user.generate_confirmation_token()
+        send_email(user.email,'Confirm you email','auth/email/confirm',user = user,token = token)
+        flash('You can confirm the email and then login')
+        return redirect(url_for('main.index'))
+    return render_template('auth/register.html', form = form)
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        db.session.commit()
+        flash('you have confirmed your account. Thanks')
+    else:
+        flash('the confirmation link is invalid or has expired')
+    return redirect(url_for('main.index'))
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, 'Confirm Your Account',
+               'auth/email/confirm', user=current_user, token=token)
+    flash('A new confirmation email has been sent to you by email.')
+    return redirect(url_for('main.index'))
